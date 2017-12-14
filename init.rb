02cfab62c4f19d4a9f9ec2c448a0b4b69796da63
Vinh Nguyen <xuanvinhct@gmail.com>
2017-12-14 20:09:42 +0700

    bump new version

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :manage_wiki_view_page_permission do
   name 'Manage Page Permission Plugin'
   author 'Systango'
   description 'This is a plugin for managing page permissions'
-  version '0.0.1'
+  version '0.0.2'
 	requires_redmine :version_or_higher => '2.2.4'
 
   project_module :wiki do
