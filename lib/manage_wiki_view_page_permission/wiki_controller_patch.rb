02b31b8805015583c6b43c6b8eb775be96ec196b
Vinh Nguyen <xuanvinhct@gmail.com>
2017-12-15 22:55:00 +0700

    remove protected page from wiki/index page

--- lib/manage_wiki_view_page_permission/wiki_controller_patch.rb
+++ lib/manage_wiki_view_page_permission/wiki_controller_patch.rb
@@ -1,13 +1,34 @@
 module ManageWikiViewPagePermission
   module WikiControllerPatch
     def self.included(base)
+      base.send(:include, InstanceMethods)
       base.class_eval do
         before_filter :validate_permission_for_protected_pages, :only=>:show
+        alias_method_chain :load_pages_for_index, :protected
         private
 
         def validate_permission_for_protected_pages
           deny_access and return if @page.protected? and !User.current.allowed_to?(:view_protected_pages, @project)
         end
+      end
+    end
+
+    module InstanceMethods
+      def load_pages_for_index_with_protected
+        @pages = if User.current.allowed_to?(:view_protected_pages, @project)
+                   @wiki.pages.with_updated_on.
+                     reorder("#{WikiPage.table_name}.title").
+                     includes(:wiki => :project).
+                     includes(:parent).
+                     to_a
+                 else
+                   @wiki.pages.with_updated_on.
+                     where(protected: false).
+                     reorder("#{WikiPage.table_name}.title").
+                     includes(:wiki => :project).
+                     includes(:parent).
+                     to_a
+                 end
       end
     end
   end
